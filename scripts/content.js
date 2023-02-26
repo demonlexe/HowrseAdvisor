f598c6b874e66c0ad3eb2882bc435252f81d012b
Alexis Kaufman <alexiskaufman@gmail.com>
2023-02-26 13:47:36 -0600

    add dummy values if user has not represented their own clicks yet

--- scripts/content.js
+++ scripts/content.js
@@ -35,6 +35,27 @@ const locStruct = {
 }
 let missionValArr = [];
 let sleepValArr = [];
+let missionValOffsetUninit = [
+    { x: 37.40625, y: 17 },
+    { x: 78.796875, y: 44 },
+    { x: 29.796875, y: 43 },
+    { x: 30.796875, y: 35 },
+    { x: 59.796875, y: 40 },
+    { x: 83.796875, y: 10 },
+    { x: 35.796875, y: 43 },
+    { x: 48.796875, y: 33 },
+    { x: 32.796875, y: 20 },
+    { x: 19.796875, y: 47 },
+    { x: 42.796875, y: 1 },
+];
+let sleepValOffsetUninit = [
+    { x: 60.5, y: 17 },
+    { x: 59.5, y: 22 },
+    { x: 36.5, y: 19 },
+    { x: 64.5, y: 43 },
+    { x: 44.5, y: 1 },
+    { x: 44.5, y: 24 },
+];
 
 const winPath = window.location.pathname;
 console.log(winPath);
@@ -117,8 +138,8 @@ function calcMeanAndStandardDeviation(ValArr, strName) {
 }
 
 async function mappingTestPoints() {
-    missionValArr = await getData("missionButtonMappings") || [];
-    sleepValArr = await getData("sleepButtonMappings") || [];
+    missionValArr = await getData("missionButtonMappings") || missionValOffsetUninit;
+    sleepValArr = await getData("sleepButtonMappings") || sleepValOffsetUninit;
 
     calcMeanAndStandardDeviation(missionValArr, "mission");
     calcMeanAndStandardDeviation(sleepValArr, "sleep");
